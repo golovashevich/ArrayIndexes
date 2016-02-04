2735ec7b9d5a81ee161814efbd7e5d02dca88dce
Maxim Golovashevich <golovashevich@gmail.com>
2016-02-04 21:19:03 +0300

    Lookup with hashes

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 namespace Calculator {
 	class Program {
 		public const int MAX_ELEMENTS = 200000;
+
 		static void Main(string[] args) {
 			var intArray = new int[MAX_ELEMENTS]; 
 			var rand = new Random();
@@ -18,21 +19,35 @@ namespace Calculator {
 			int index1 = rand.Next(intArray.Length);
 			int index2 = rand.Next(intArray.Length);
 
+			//worth case for brute
+			index1 = intArray.Length - 2; 
+			index2 = intArray.Length - 1; 
+
 			int targetSum = intArray[index1] + intArray[index2];
 
 			try {
-				var start = DateTime.Now;
-				Tuple<int, int> indexes = CalculateIndexesForSum_Brute(intArray, targetSum);
-				var duration = DateTime.Now - start;
-				Console.WriteLine("Time to calculate: {0}", duration.TotalSeconds);
-				Console.WriteLine("Expected\tindex1 - {0}\tindex2 - {1}\ttargetSum: {2}", index1, index2, targetSum);
-				Console.WriteLine("Brute lookup\tindex1 - {0}\tindex2 - {1}\tconrolSum: {2}",indexes.Item1, indexes.Item2, 
-						intArray[indexes.Item1] + intArray[indexes.Item2]);
+				Console.WriteLine("Lookup algoritms estimation");
+
+				EstimateAlgorithm(intArray, index1, index2, targetSum, CalculateIndexesForSum_Brute, "Brute lookup");
+				EstimateAlgorithm(intArray, index1, index2, targetSum, CalculateIndexesForSum_Hashes, "Hashes lookup");
+
+				Console.WriteLine("Press Enter to continue...");
 				Console.ReadLine();
 			}
 			catch (Exception ex) {
 				Console.WriteLine(ex.Message);
 			}
+		}
+
+		private static void EstimateAlgorithm(int[] intArray, int index1, int index2, int targetSum, 
+				Func<int[], int, Tuple<int, int>> calculator, string p) {
+			var start = DateTime.Now;
+			Tuple<int, int> indexes = calculator(intArray, targetSum);
+			var duration = DateTime.Now - start;
+			Console.WriteLine("\nTime to calculate: {0} msec(s)", duration.TotalMilliseconds);
+			Console.WriteLine("Expected\tindex1 - {0}\tindex2 - {1}\ttargetSum: {2}", index1, index2, targetSum);
+			Console.WriteLine("Brute lookup\tindex1 - {0}\tindex2 - {1}\tconrolSum: {2}", indexes.Item1, indexes.Item2,
+					intArray[indexes.Item1] + intArray[indexes.Item2]);
 		}
 
 		private static Tuple<int, int> CalculateIndexesForSum_Brute(int[] intArray, int targetSum) {
@@ -41,6 +56,22 @@ namespace Calculator {
 				for (int j = i; j < intArray.Length; j++) {
 					if (intArray[j] == remainder) {
 						return Tuple.Create(i, j);
+					}
+				}
+			}
+			throw new Exception("No indexes have been found");
+		}
+
+		private static Tuple<int, int> CalculateIndexesForSum_Hashes(int[] intArray, int targetSum) {
+			var hashes = new HashSet<int>(intArray);
+			for (int i = 0; i < intArray.Length; i++) {
+				var remainder = targetSum - intArray[i]; 
+				if (hashes.Contains(remainder)) {
+					//Lookup for an specific element only one time
+					for (int j = 0; j < intArray.Length; j++) {
+						if (intArray[j] == remainder) {
+							return Tuple.Create(i, j);
+						}
 					}
 				}
 			}
